d6de5a0ea38f12a4a7877e0bff9a11bf49ffb4df
Allan Chua <allanchua.officefiles@gmail.com>
2022-06-03 20:24:35 +0800

    Build diagram

--- scripts/003_build.sh
+++ scripts/003_build.sh
@@ -21,11 +21,11 @@ if [ -z $MAIN_DISTRIBUTION_ID ]; then
   exit 1
 fi
 
-# cd ../react-app
+cd ../react-app
 
-# npm run build
+npm run build
 
-# cd ./build
+cd ./build
 
 MAIN_BUCKET_NAME="${PROJECT_NAME}-${ENV_NAME}-website-main-s3"
 FAILOVER_BUCKET_NAME="${PROJECT_NAME}-${ENV_NAME}-website-failover-s3"
@@ -33,7 +33,7 @@ FAILOVER_BUCKET_NAME="${PROJECT_NAME}-${ENV_NAME}-website-failover-s3"
 echo $MAIN_BUCKET_NAME
 echo $FAILOVER_BUCKET_NAME
 
-# aws s3 sync . "s3://${MAIN_BUCKET_NAME}" --delete --profile $AWS_PROFILE
-# aws s3 sync . "s3://${FAILOVER_BUCKET_NAME}" --delete --profile $AWS_PROFILE
+aws s3 sync . "s3://${MAIN_BUCKET_NAME}" --delete --profile $AWS_PROFILE
+aws s3 sync . "s3://${FAILOVER_BUCKET_NAME}" --delete --profile $AWS_PROFILE
 
-# aws cloudfront create-invalidation --distribution-id $MAIN_DISTRIBUTION_ID --paths "/*" --profile $AWS_PROFILE
+aws cloudfront create-invalidation --distribution-id $MAIN_DISTRIBUTION_ID --paths "/*" --profile $AWS_PROFILE
