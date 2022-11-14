10a72c0ea427be86d6cf28d8ab9e48db2c68a1fe
Allan Chua <allanchua.officefiles@gmail.com>
2022-11-14 22:25:27 +0800

    Update disarm-cache-headers.tf

--- terraform/disarm-cache-headers.tf
+++ terraform/disarm-cache-headers.tf
@@ -43,29 +43,33 @@ resource "aws_cloudfront_response_headers_policy" "disarm_cache_headers_policy" 
     content_type_options {
       override = true
     }
+    
     frame_options {
       frame_option = "DENY"
       override     = true
     }
+    
     referrer_policy {
       referrer_policy = "same-origin"
       override        = true
     }
+    
     xss_protection {
       mode_block = true
       protection = true
       override   = true
     }
+    
     strict_transport_security {
       access_control_max_age_sec = "63072000"
       include_subdomains         = true
       preload                    = true
       override                   = true
     }
-    # Research CSP
-    # content_security_policy {
-    #   content_security_policy = ""
-    #   override                = true
-    # }
+    
+    content_security_policy {
+      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+      override = true
+    }
   }
 }
